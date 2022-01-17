cab08d3e4f6c5b4197a61fc9d0dc42f15cd03404
sanjaykashyap80 <91534908+sanjaykashyap80@users.noreply.github.com>
2022-01-17 17:05:31 +0530

    eval-2

---
+++ eval-2/src/App.js
@@ -0,0 +1,77 @@
+import data from "./data.json";
+import Jobs from "./components/Jobs";
+import { useState } from "react";
+import Header from "./components/Header";
+
+
+function App() {
+  const [filterKeywords, setfilterKeywords] = useState([]);
+
+  const addFilterKeywords = (data) => {
+    if (!filterKeywords.includes(data)) {
+      setfilterKeywords([...filterKeywords, data]);
+    }
+  };
+
+  const deleteKeyword = (data) => {
+    const newKeywords = filterKeywords.filter((key) => key !== data);
+    setfilterKeywords(newKeywords);
+  };
+
+  const clearAll = () => {
+    setfilterKeywords([]);
+  };
+
+  return (
+    <div>
+      <h1>Available Jobs</h1>
+      <div className="header"></div>
+
+      {/* <Search setSearchKeyword={setSearchKeyword} /> */}
+
+      {filterKeywords.length > 0 && (
+        <Header
+          keywords={filterKeywords}
+          removeKeywords={deleteKeyword}
+          clearAll={clearAll}
+        />
+      )}
+
+      <Jobs
+        keywords={filterKeywords}
+        data={data}
+        setKeywords={addFilterKeywords}
+      />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
